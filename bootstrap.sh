6f6fac57b75071c9573c5080341ea69b27a8c6b4
Federico Gimenez <fgimenez@coit.es>
2014-12-30 15:52:29 +0100

    emacs conf

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,6 +16,7 @@ vagrant up --provision
 vagrant sh -c 'cd ~/devstack && ./stack.sh'
 vagrant sh -c "git config --global user.email '$GIT_EMAIL'"
 vagrant sh -c "git config --global user.name '$GIT_NAME'"
+vagrant sh -c 'rm -rf ~/.emacs.d && git clone https://github.com/fgimenez/.emacs.d.git ~/.emacs.d'
 
 vagrant sh -c 'mkdir -p ~/.ssh'
 scp -i ~/.vagrant.d/insecure_private_key -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ~/.ssh/id_rsa* vagrant@devstack.local:/home/vagrant/.ssh
